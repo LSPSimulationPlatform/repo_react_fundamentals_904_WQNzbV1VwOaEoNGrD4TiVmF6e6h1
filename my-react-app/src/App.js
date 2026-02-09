d5ae56a664dcc22e26d50e30b1fd2c7a298e58d7
turanaydinliilgar-hub <turanaydinli.ilgar@gmail.com>
2026-02-09 18:52:47 +0000

    react project update

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <label style={fontSize:"30px",}}>It's my first project.</label
+    </div>
+  );
+}
+
+export default App;
