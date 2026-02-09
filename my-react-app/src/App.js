f92cd74c9a8b2d96d0a45c919c9b103fe765116c
turanaydinliilgar-hub <turanaydinli.ilgar@gmail.com>
2026-02-09 19:03:24 +0000

    react project update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <label style={fontSize:"30px",}}>It's my first project.</label
+      <label style={{ fontSize: "30px", color: "red" }}>
+        It's my first project.
+      </label>
     </div>
   );
 }
